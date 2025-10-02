f5e9f5123ed9dd10cf5c7a6c3673fa6fd2c5d235
ENR <enr00005@gmail.com>
2025-10-02 15:13:45 +0530

    Emotion detection

---
+++ emotion/emotiondetection.py
@@ -0,0 +1,54 @@
+import cv2
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array
+
+# Load the pre-trained Mini-Xception model
+# model = load_model('mini_XCEPTION_model.hdf5')
+
+# Load model for inference only
+model = load_model('mini_XCEPTION_model.hdf5', compile=False)
+
+
+# Emotion labels
+emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+
+# Load Haar cascade for face detection
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# Start webcam
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+
+    for (x, y, w, h) in faces:
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_gray = cv2.resize(roi_gray, (64, 64))  # model input size
+        roi_gray = roi_gray.astype("float") / 255.0
+        roi_gray = img_to_array(roi_gray)
+        roi_gray = np.expand_dims(roi_gray, axis=0)
+
+        # Predict emotion
+        preds = model.predict(roi_gray, verbose=0)[0]
+        emotion = emotion_labels[np.argmax(preds)]
+
+        # Draw rectangle and label
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.putText(frame, emotion, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0,0,255), 2)
+        
+         # **Print detected emotion in terminal**
+        print(f"Detected Emotion: {emotion}")
+
+    cv2.imshow('Emotion Detection', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
